a209d9eed0fee97a59015d3a6cbb07d2fc903e48
Bart Lantz <underhilllabs@gmail.com>
2015-11-20 08:07:55 -0700

    Create .lein/profile.clj

---
+++ .lein/profiles.clj
@@ -0,0 +1,4 @@
+{:user {:plugins [[cider/cider-nrepl "0.9.1"]                                                                                                                  
+                  [lein-ancient "0.6.8"]                                                                                                                       
+                  [lein-kibit "0.1.2"]                                                                                                                         
+                  [lein-pprint "1.1.2"]]}}
